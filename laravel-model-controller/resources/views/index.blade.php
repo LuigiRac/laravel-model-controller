b85a533877f9443d5d94a8e6148ddffc6ec1e47e
LuigiRac <lui.rac06@gmail.com>
2025-05-02 12:25:12 +0200

    import movies dal database

--- laravel-model-controller/resources/views/index.blade.php
+++ laravel-model-controller/resources/views/index.blade.php
@@ -1,2 +1,11 @@
 @extends('layouts.app')
 
+@section('content')
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                {{$movie}}
+            </li>
+        @endforeach
+    </ul>
+@endsection
